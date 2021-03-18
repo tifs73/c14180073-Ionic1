b6bb69bfec2b5af7aff4f025a5fee2278521b1ec
TifS <c14180073@john.petra.ac.id>
2021-03-18 23:30:44 +0700

    done

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,14 +12,15 @@ export class Tab2Page {
 
   tebak : number;
   output : string;
-  srnd : number = Math.floor(Math.random() * 10);;
+  srnd : number = Math.floor(Math.random() * 10);
+  foto : boolean = false;
 
   random() {
     var counter = 0;
     if (counter == 0) {
       if (this.tebak == this.srnd) {
         this.output = "Jawaban berhasil di tebak!";
-
+        this.foto = true;
       } else if (this.tebak < this.srnd) {
         this.output = "Jawaban terlalu kecil"
       } else if (this.tebak > this.srnd) {
